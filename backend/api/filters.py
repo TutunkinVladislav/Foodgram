a534dbb4117a6804733f33c2f046b0599b4ebe7e
Тутункин Владислав <vladislav-login94@yandex.ru>
2023-07-20 11:58:41 +0300

    update filters.py

--- backend/api/filters.py
+++ backend/api/filters.py
@@ -15,10 +15,7 @@ class IngredientFilter(FilterSet):
 class RecipesFilter(FilterSet):
     """Фильтр для рецептов"""
     tags = filters.AllValuesMultipleFilter(field_name='tags__slug')
-    author = filters.Filter(field_name='author__id')
-    is_favorited = filters.BooleanFilter(
-        method='get_is_favorited'
-    )
+    is_favorited = filters.Filter(field_name='favorite')
     is_in_shopping_cart = filters.BooleanFilter(
         method='get_is_in_shopping_cart',
     )
