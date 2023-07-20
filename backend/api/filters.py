9c3df155613855cdcc6ee18b41469b91bbcae463
Тутункин Владислав <vladislav-login94@yandex.ru>
2023-07-20 11:31:39 +0300

    update filters.py

--- backend/api/filters.py
+++ backend/api/filters.py
@@ -15,7 +15,9 @@ class IngredientFilter(FilterSet):
 class RecipesFilter(FilterSet):
     """Фильтр для рецептов"""
     tags = filters.AllValuesMultipleFilter(field_name='tags__slug')
-    is_favorited = filters.Filter(field_name='is_favorited')
+    is_favorited = filters.BooleanFilter(
+        method='get_is_favorited'
+    )
     is_in_shopping_cart = filters.BooleanFilter(
         method='get_is_in_shopping_cart',
     )
@@ -23,6 +25,12 @@ class RecipesFilter(FilterSet):
     class Meta:
         model = Recipe
         fields = ['is_favorited', 'author', 'tags', 'is_in_shopping_cart']
+
+    def get_is_favorited(self, queryset, name, value):
+        user = self.request.user
+        if value and user.is_authenticated:
+            return queryset.filter(favorite__user=user)
+        return queryset
 
     def get_is_in_shopping_cart(self, queryset, name, value):
         user = self.request.user
